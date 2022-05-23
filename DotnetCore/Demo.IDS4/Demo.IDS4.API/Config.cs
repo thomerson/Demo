a86fab4d81630a33507d37d7b2e9cf7a5b73f815
thomerson <756155037@qq.com>
2022-05-23 22:14:26 +0800

    IdentityServer4

---
+++ DotnetCore/Demo.IDS4/Demo.IDS4.API/Config.cs
@@ -0,0 +1,22 @@
+﻿using IdentityServer4.Models;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Demo.IDS4.API
+{
+    public static class Config
+    {
+        public static IEnumerable<ApiResource> Apis => new List<ApiResource> {
+            new ApiResource("IDS4.api")
+        };
+
+        public static IEnumerable<Client> Clients => new List<Client> {
+            new Client(){
+                ClientId =  "ClientA",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                AllowedScopes = new List<string> { "IDS4.api" },
+                ClientSecrets  = { new Secret("secret".Sha256())}
+            }
+        };
+    }
+}
---
+++ DotnetCore/Demo.IDS4/Demo.IDS4.API/Controllers/IdentityController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Demo.IDS4.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IdentityController : ControllerBase
+    {
+        public JsonResult Get()
+        {
+            var result = User.Claims.Select(s => new { s.Type, char.MinValue });
+
+            return new JsonResult(result);
+        }
+    }
+}
