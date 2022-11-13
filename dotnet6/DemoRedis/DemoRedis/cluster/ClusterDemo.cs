75e8d718b98fc01c282b2a2cfcf5e80f13654dde
thomerson <756155037@qq.com>
2022-11-13 19:42:48 +0800

    redis

---
+++ dotnet6/DemoRedis/DemoRedis/cluster/ClusterDemo.cs
@@ -0,0 +1,18 @@
+﻿using StackExchange.Redis;
+
+namespace DemoRedis.cluster
+{
+    internal class ClusterDemo
+    {
+
+        public void Test()
+        {
+            // reids集群,逗号分隔服务器
+            var redisClient = ConnectionMultiplexer.Connect("127.0.0.1:6379,127.0.0.1:6380,127.0.0.1:6381,127.0.0.1:6382");
+
+            var db = redisClient.GetDatabase();
+
+            // 参考RedisDemo操作
+        }
+    }
+}
