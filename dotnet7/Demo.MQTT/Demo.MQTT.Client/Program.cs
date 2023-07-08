608085099b7fc0c1b15041f067b822e15552dbaa
thomerson <756155037@qq.com>
2023-07-08 12:00:50 +0800

    MQTT

---
+++ dotnet7/Demo.MQTT/Demo.MQTT.Client/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MQTTnet.Client;
+using MQTTnet;
+using System.Text;
+
+await MQTTClientTest();
+
+Console.WriteLine("Hello, World!");
+
+Console.ReadLine();
+
+
+static async Task MQTTClientTest()
+{
+    var factory = new MqttFactory();
+    var client = factory.CreateMqttClient();
+    var options = new MqttClientOptionsBuilder()
+        .WithTcpServer("localhost", 1883)
+        .WithCredentials("lotClient", "lotClient")
+        .Build();
+    client.ApplicationMessageReceivedAsync += Client_ApplicationMessageReceivedAsync;
+    await client.ConnectAsync(options);
+    await client.SubscribeAsync(new MqttTopicFilterBuilder().WithTopic("testTopic").Build());
+}
+
+static Task Client_ApplicationMessageReceivedAsync(MqttApplicationMessageReceivedEventArgs arg)
+{
+    Console.WriteLine($"接收的消息: {arg.ApplicationMessage.ConvertPayloadToString()}");
+    return Task.CompletedTask;
+}
---
+++ dotnet7/Demo.MQTT/Demo.MQTT.Server/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MQTTnet.Client;
+using MQTTnet;
+using System.Text;
+
+await MQTTP();
+
+Console.WriteLine("Hello, World!");
+
+
+
+static async Task MQTTP()
+{
+    var factory = new MqttFactory();
+    var client = factory.CreateMqttClient();
+
+    var options = new MqttClientOptionsBuilder()
+        .WithTcpServer("localhost", 1883)
+        .WithCredentials("lotServer", "lotServer")
+        .Build();
+
+    await client.ConnectAsync(options);
+    while (true)
+    {
+        Console.WriteLine("输入要发布的信息: ");
+        var message = Console.ReadLine();
+
+        var mqttMessage = new MqttApplicationMessageBuilder()
+            .WithTopic("testTopic")
+            .WithPayload(Encoding.UTF8.GetBytes(message))
+            .WithQualityOfServiceLevel(MQTTnet.Protocol.MqttQualityOfServiceLevel.ExactlyOnce)
+            .Build();
+
+        await client.PublishAsync(mqttMessage);
+    }
+}
